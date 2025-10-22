0d6ec3ed1fa18bcfe3cbcdf8a8b64e972b350378
AlessandroCaula <alecaula96@gmail.com>
2025-10-22 19:48:49 +0200

    - Added some motions on the about me component with framer motion - Add typing motion to the home text with react typing animation

--- src/components/custom/AboutCard.tsx
+++ src/components/custom/AboutCard.tsx
@@ -1,6 +1,7 @@
 import { ExternalLink } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 interface props {
   icon?: string;
@@ -14,7 +15,13 @@ const AboutCard = ({ icon, imgPath, title, text, link = "" }: props) => {
   const [customCursor, setCustomCursor] = useState(false);
 
   return (
-    <div className={`about-card ${customCursor ? "custom-cursor" : ""}`}>
+    <motion.div 
+      className={`about-card ${customCursor ? "custom-cursor" : ""}`}
+      initial={{ opacity: 0, y: 50 }}   // The card starts invisible and shifted 50px down.
+      whileInView={{ opacity: 1, y: 0 }}  // When the card scrolls into view, it fades in and slides up.
+      transition={{ duration: 0.7, ease: "easeOut" }} // Defines the timing curve.
+      viewport={{ once: true, amount:0.3 }} // Means the animation triggers once, when 30% of the card enters the viewport. So it doesn't re-trigger when scrolling up and down.
+    >
       <div className="grid grid-cols-3 grid-rows-1 items-center">
         <div className="flex justify-start items-center">
           {imgPath ? (
@@ -41,7 +48,7 @@ const AboutCard = ({ icon, imgPath, title, text, link = "" }: props) => {
           </Link>
         </div>
       )}
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,48 +1,89 @@
-import { BsGithub, BsLinkedin } from 'react-icons/bs'
-import LightModeProfilePic from '../../assets/Web_Light_Mode.jpeg'
-import DarkModeProfilePic from '../../assets/Web_Dark_Mode.jpeg'
-import SocialButton from '../custom/SocialButton'
-import { SiGmail } from 'react-icons/si'
-import { GrDocumentPdf } from 'react-icons/gr'
-import { useTheme } from '@/context/ThemeContext'
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import LightModeProfilePic from "../../assets/Web_Light_Mode.jpeg";
+import DarkModeProfilePic from "../../assets/Web_Dark_Mode.jpeg";
+import SocialButton from "../custom/SocialButton";
+import { SiGmail } from "react-icons/si";
+import { GrDocumentPdf } from "react-icons/gr";
+import { useTheme } from "@/context/ThemeContext";
+import { TypeAnimation } from "react-type-animation";
+import { useState } from "react";
 
 const Home = () => {
   const { isDark } = useTheme();
+  const [showSecondLine, setShowSecondLine] = useState(false);
 
   return (
-    <div className='flex-center flex-col margin-top'>
-      <img 
-        className="w-68 custom-shadow rounded-full max-sm:w-56" 
-        src={isDark ? DarkModeProfilePic : LightModeProfilePic} 
-        alt="Alessandro Caula Picture" 
+    <div className="flex-center flex-col margin-top">
+      <img
+        className="w-68 custom-shadow rounded-full max-sm:w-56"
+        src={isDark ? DarkModeProfilePic : LightModeProfilePic}
+        alt="Alessandro Caula Picture"
       />
-      <h1 className='sm:mt-18 mt-12'>Hi! I'm <span className='font-inter'>Alessandro Caula</span></h1>
-      <h2 className='font-inter sm:mt-8 mt-6 font-light'>Software Developer and Bioinformatician</h2>
 
-      <div className='mt-14 sm:mt-20 w-full flex-center sm:space-x-4 md:space-x-8 max-sm:flex-col max-sm:space-y-4 max-sm:px-4'>
-        <SocialButton 
+      {/* <h1 className="sm:mt-18 mt-12">
+        Hi! I'm <span className="font-inter">Alessandro Caula</span>
+      </h1>
+      <h2 className="font-inter sm:mt-8 mt-6 font-light">
+        Software Developer and Bioinformatician
+      </h2> */}
+
+      {/* Animate text */}
+      <TypeAnimation
+        sequence={["Hi! I'm Alessandro Caula", () => setShowSecondLine(true)]}
+        wrapper="h1"
+        speed={30}
+        cursor={false}
+        repeat={0} // Avoid looping
+        className="sm:mt-18 mt-12"
+      />
+      {/* Second line animation */}
+      {/* Container that reserves the space for the second line animation */}
+      <div className="h-[calc(2em+0.5rem)] sm:h-[calc(2.5em+0.5rem)] md:h-[calc(3em+0.5rem)]">
+        {showSecondLine && (
+          <TypeAnimation
+            sequence={["Software Developer and Bioinformatician", 500]}
+            wrapper="h2"
+            speed={50}
+            cursor={false}
+            repeat={0} // Avoid looping
+            className="font-inter sm:mt-8 mt-6 font-light"
+          />
+        )}
+      </div>
+
+      <div className="mt-14 sm:mt-20 w-full flex-center sm:space-x-4 md:space-x-8 max-sm:flex-col max-sm:space-y-4 max-sm:px-4">
+        <SocialButton
           icon={<BsLinkedin className={!isDark ? "text-[#0967C2]" : ""} />}
-          label='LinkedIn'
-          onClick={() => window.open('https://www.linkedin.com/in/alessandro-caula-25a20418a/', '_blank')}
+          label="LinkedIn"
+          onClick={() =>
+            window.open(
+              "https://www.linkedin.com/in/alessandro-caula-25a20418a/",
+              "_blank"
+            )
+          }
         />
-        <SocialButton 
+        <SocialButton
           icon={<BsGithub />}
-          label='GitHub'
-          onClick={() => window.open('https://github.com/AlessandroCaula', '_blank')}
+          label="GitHub"
+          onClick={() =>
+            window.open("https://github.com/AlessandroCaula", "_blank")
+          }
         />
-        <SocialButton 
+        <SocialButton
           icon={<SiGmail className={!isDark ? "text-[#EA4336]" : ""} />}
-          label='Email'
-          onClick={() => window.location.href = 'mailto:alecaula96@gmail.com'}
+          label="Email"
+          onClick={() => (window.location.href = "mailto:alecaula96@gmail.com")}
         />
-        <SocialButton 
+        <SocialButton
           icon={<GrDocumentPdf />}
-          label='CV'
-          onClick={() => window.open('/public/Alessandro_Caula_CV.pdf', '_blank')}
+          label="CV"
+          onClick={() =>
+            window.open("/public/Alessandro_Caula_CV.pdf", "_blank")
+          }
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
