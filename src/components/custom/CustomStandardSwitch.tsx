29eb05da4e7682712cf26200a1b8d927c03cad92
AlessandroCaula <alecaula96@gmail.com>
2025-10-27 16:42:33 +0100

    changed pointer

--- src/components/custom/CustomStandardSwitch.tsx
+++ src/components/custom/CustomStandardSwitch.tsx
@@ -12,7 +12,7 @@ const CustomStandardSwitch = ({ checked, onCheckedChange, className = "" }: prop
       <Switch
         className="
           h-5 w-9 shadow-sm shadow-slate-400 ring-1 ring-slate-500/10
-          data-[state=checked]:[&>span]:translate-x-[calc(20px-2px)]
+          data-[state=checked]:[&>span]:translate-x-[calc(20px-2px)] cursor-pointer
           data-[state=checked]:bg-orange-400 data-[state=unchecked]:[&span]:bg-white data-[state=unchecked]:bg-slate-100
           [&>span]:bg-slate-200 [&>span]:shadow-sm [&>span]:shadow-slate-500
         "
