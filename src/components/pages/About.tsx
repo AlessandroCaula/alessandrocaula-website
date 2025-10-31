4e19a319f4b73a13f53b4c8a6037c10946fb0747
AlessandroCaula <alecaula96@gmail.com>
2025-10-31 14:58:36 +0100

    Updated About Me section text

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -11,11 +11,11 @@ const About = () => {
         title="About Me"
         text={
           <p className="flex flex-grow items-center font-inter text-center text-foreground md:text-[18px] leading-7">
-            I'm a software developer and bioinformatician with a background in computational biology. I enjoy working where science meets software and love solving real problems with code. I’ve learned to treat data with care and build tools that are fast, clean, and useful.
+            I'm a software developer and bioinformatician with a background in computational biology. I enjoy working where science meets software and love solving real problems with code.
             <br />
-            <br />Curious by nature and always eager to learn, I enjoy reading about finance and personal growth, spending time outdoors, hanging out with friends, or staying active with kickboxing. 
+            <br />I'm naturally curious and enjoy reading about finance and personal growth, spending time outdoors, hanging out with friends, and staying active with kickboxing. 
             <br />
-            <br />I love exploring what fascinates me, diving into new technologies, and following my curiosity wherever it leads.
+            <br />I like experimenting with new technologies and figuring out how things work, whether it's for a project, a problem, or just to learn something new.
           </p>
         }
       />
